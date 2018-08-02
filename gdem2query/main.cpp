a8d66e9a867ce7be5800e80f39ec0a8c375bb5e5
iclosure <iclosure@users.noreply.github.com>
2018-08-02 17:32:11 +0800

    增加测试项

--- gdem2query/main.cpp
+++ gdem2query/main.cpp
@@ -4,8 +4,8 @@
 
 int main(int argc, char **argv)
 {
-	//
 	int height = gdem2_query_point(30.5763307666, 104.0712219292);
+	//int height = gdem2_query_point(30.71361111111111, 104.00277777777778);
 
 	return 0;
 }
